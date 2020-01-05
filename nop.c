8194a4f96dd0b453ee51cf777a472850fd258789
ArthurAbeshouse <artieabe99@gmail.com>
2020-01-05 21:15:02 +0000

    fixed nop

--- nop.c
+++ nop.c
@@ -7,6 +7,13 @@
 
 void nop(stack_t **stack, unsigned int line_num)
 {
-	(void)stack;
-	(void)line_num;
+  int i = 1;
+  (void)stack;
+  (void)line_num;
+
+  while (i >= 0)
+    {
+      i--;
+      continue;
+    }
 }
